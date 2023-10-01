fef70ca4568beddd04e2b65b91c64beebbdbbd0a
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-01 13:58:31 +0200

    Configure webpack, prettier and eslint

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import path from 'path';
+
+const config = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve('dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
+
+export default config;
