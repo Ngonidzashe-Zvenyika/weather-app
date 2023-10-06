428ae932c412f4192c77f4635090510b46a64220
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-06 10:58:19 +0200

    Update how data recieved from the api is processed and returned

--- src/api-functions/metric.js
+++ src/api-functions/metric.js
@@ -1,15 +1,23 @@
 import { format, parseISO } from 'date-fns';
 
+// This function converts the time value of a given date so that it matches the format 00:00 if it arrives in the format 0:00, this is so that it can be manipulated by date-fns;
+function convertTime(date) {
+  let substringOne = date.substring(0, 11);
+  substringOne += 0;
+  const substringTwo = date.substring(11);
+  date = substringOne + substringTwo;
+  return date;
+}
+
+// This function uses date-fns to determine what day a certain date falls on and formats the day and time accordingly;
 function formatDate(date) {
   if (date.length < 16) {
-    let substringOne = date.substring(0, 11);
-    substringOne += 0;
-    const substringTwo = date.substring(11);
-    date = substringOne + substringTwo;
+    date = convertTime(date);
   }
   return format(parseISO(date), 'EE, HH:mm');
 }
 
+// This function converts a numerical uv-index reading into a lexical estimation;
 function determineUvIndex(uvIndex) {
   switch (true) {
     case uvIndex < 3:
@@ -31,47 +39,43 @@ function determineUvIndex(uvIndex) {
   return uvIndex;
 }
 
+// This function extracts the relevant keys containing information about the current weather from the data or today objects and returns a single formatted object containing the information the application will use whilst disregarding all other data;
 function extractCurrentWeather(data, today) {
   const currentWeather = {
     date: formatDate(data.location.localtime),
     location: data.location.name,
     country: data.location.country,
     condition: data.current.condition,
-    feelsLike: `${data.current.feelslike_c}°`,
+    feelsLike: `${Math.round(data.current.feelslike_c)}°`,
     humidity: `${data.current.humidity}%`,
-    temp: `${data.current.temp_c}°`,
+    temp: `${Math.round(data.current.temp_c)}°`,
     uvIndex: determineUvIndex(data.current.uv),
-    wind: `${data.current.wind_kph} km/h`,
+    wind: `${Math.round(data.current.wind_kph)} km/h`,
     sunrise: today.astro.sunrise,
     sunset: today.astro.sunset,
-    minTemp: `${today.day.mintemp_c}°`,
-    maxTemp: `${today.day.maxtemp_c}°`,
   };
   return currentWeather;
 }
 
+// This function uses date-fns to determine the current hour from the time value within a given date;
 function determineCurrentHour(date) {
   if (date.length < 16) {
-    let substringOne = date.substring(0, 11);
-    substringOne += 0;
-    const substringTwo = date.substring(11);
-    date = substringOne + substringTwo;
+    date = convertTime(date);
   }
   return format(parseISO(date), 'H');
 }
 
-function determineHourlyForecast(data, today, tomorrow) {
-  const currentHour = determineCurrentHour(data.location.localtime);
+// This function extracts the relevant keys containing information about the hourly forecasts from the today or tomorrow objects (24hr period) and returns an array containing formatted objects that hold the information that the application will use whilst disregarding all other data;
+function determineHourlyForecast(localTime, today, tomorrow) {
+  const currentHour = determineCurrentHour(localTime);
   const hourlyForecast = [];
   today.hour.forEach((hour, index) => {
     if (index >= currentHour) {
       hourlyForecast.push({
         chanceOfRain: `${hour.chance_of_rain}%`,
         conditon: hour.condition,
-        feelsLike: `${hour.feelslike_c}°`,
         time: format(parseISO(hour.time), 'HH:mm'),
-        temp: `${hour.temp_c}°`,
-        wind: `${hour.wind_kph} km/h`,
+        temp: `${Math.round(hour.temp_c)}°`,
       });
     }
   });
@@ -81,10 +85,8 @@ function determineHourlyForecast(data, today, tomorrow) {
         hourlyForecast.push({
           chanceOfRain: `${hour.chance_of_rain}%`,
           conditon: hour.condition,
-          feelsLike: `${hour.feelslike_c}°`,
           time: format(parseISO(hour.time), 'HH:mm'),
-          temp: `${hour.temp_c}°`,
-          wind: `${hour.wind_kph} km/h`,
+          temp: `${Math.round(hour.temp_c)}°`,
         });
       }
     });
@@ -92,26 +94,28 @@ function determineHourlyForecast(data, today, tomorrow) {
   return hourlyForecast;
 }
 
+// This function extracts the relevant keys from each index in the forecast array and returns an array containing formatted objects that hold the information the application will use whilst disregarding all other data;
 function extractFutureWeather(forecast) {
   const futureWeather = [];
-  forecast.forEach((day, index) => {
+  forecast.forEach((period, index) => {
     let dayOfWeek;
     if (index === 0) {
       dayOfWeek = 'Today';
     } else {
-      dayOfWeek = format(parseISO(day.date), 'EEEE');
+      dayOfWeek = format(parseISO(period.date), 'EEEE');
     }
     futureWeather.push({
-      condition: day.day.condition,
-      chanceOfRain: `${day.day.daily_chance_of_rain}%`,
-      minTemp: `${day.day.mintemp_c}°`,
-      maxTemp: `${day.day.maxtemp_c}°`,
+      condition: period.day.condition,
+      chanceOfRain: `${period.day.daily_chance_of_rain}%`,
+      minTemp: `${Math.round(period.day.mintemp_c)}°`,
+      maxTemp: `${Math.round(period.day.maxtemp_c)}°`,
       dayOfWeek,
     });
   });
   return futureWeather;
 }
 
+// This function attempts to retrieve data from the weather api, upon success it processes the data and returns an object, upon failure it logs the error and does not return a value;
 async function getForecastWeatherMetric(location) {
   try {
     const response = await fetch(
@@ -120,13 +124,14 @@ async function getForecastWeatherMetric(location) {
     );
     const data = await response.json();
     const today = data.forecast.forecastday[0];
-    const tomorrow = data.forecast.forecastday[1];
     const currentWeather = extractCurrentWeather(data, today);
-    const hourlyForecast = determineHourlyForecast(data, today, tomorrow);
+    const localTime = data.location.localtime;
+    const tomorrow = data.forecast.forecastday[1];
+    const hourlyForecast = determineHourlyForecast(localTime, today, tomorrow);
     const futureWeather = extractFutureWeather(data.forecast.forecastday);
     return { currentWeather, hourlyForecast, futureWeather };
   } catch (error) {
-    console.log('Invalid Location / Server Down');
+    console.log(error);
   }
 }
 
